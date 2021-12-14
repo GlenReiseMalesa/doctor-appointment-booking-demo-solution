94212cdf9386b6da296acc1a1fed33dcf214e285
GlenReiseMalesa <malesaglen6@gmail.com>
2021-12-14 23:06:03 +0200

    more changes to the home screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
-import 'package:flutter/material.dart';
+// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
 
+import 'package:flutter/material.dart';
 
 import 'helpers/helpers.dart';
 
@@ -34,65 +35,79 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-              bottomNavigationBar: footer(),
-              body: SafeArea(
-              child: LayoutBuilder(builder: (context, constraints) {
-
-                if (constraints.maxWidth < 720) {
-                  //if it's a phone screen
-                  return Column(
-                    children: <Widget>[
-                      header(5.0),
-                      const SizedBox( height: 6.0,),
-                      body(20.0),
-                    ],
-                  );
-                } else {
-                  //if it's a tablet or pc screen
-                  return Column(
-                    children: <Widget>[
-                      header(5.0),
-                    ],
-                  );
-                }
-
-
-                }),
-              ),
-
-     );
+      bottomNavigationBar: footer(),
+      body: SafeArea(
+        child: LayoutBuilder(builder: (context, constraints) {
+          if (constraints.maxWidth < 720) {
+            //if it's a phone screen
+            return Column(
+              children: <Widget>[
+                header(5.0),
+                SizedBox(height: 6.0,),
+                search(20.0),
+                flexbox(10.0),
+              ],
+            );
+          } else {
+            //if it's a tablet or pc screen
+            return Column(
+              children: <Widget>[
+                header(5.0),
+              ],
+            );
+          }
+        }),
+      ),
+    );
   }
 
   //child widgets
   Container header(var padding) {
     return Container(
-            padding: EdgeInsets.all(padding),
-            
-            child: const Text('Find Your Doctor'),
-           );
+      padding: EdgeInsets.all(padding),
+      child: const Text('Find Your Doctor'),
+    );
   }
 
-  Container body(var padding) {
+  Container search(var padding) {
     return Container(
-             
-             height: 180,
-             padding: EdgeInsets.all(padding),
-            //A RenderFlex overflowed by 99920 pixels on the bottom:solution->wrap widget with SingleChildScrollView
-            child: SingleChildScrollView(child: Form(child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.stretch,
-                                children:  <Widget>[
-                                  const SizedBox(height: 10.0,),
-                                  TextFormField(
-                                    autofocus: false,
-                                    validator: (value) => validateEmail(value!),
-                                    decoration: buildInputDecoration("SEARCH",Icons.search),
-                                  ),
-                                ],
-                               ),        
-                   ),
+      height: 180,
+      padding: EdgeInsets.all(padding),
+      //A RenderFlex overflowed by 99920 pixels on the bottom:solution->wrap widget with SingleChildScrollView
+      child: SingleChildScrollView(
+        child: Form(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              const SizedBox(
+                height: 10.0,
               ),
-           );
+              TextFormField(
+                autofocus: false,
+                validator: (value) => validateEmail(value!),
+                decoration: buildInputDecoration("SEARCH", Icons.search),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 
-
+  Container flexbox(var padding) {
+    return Container(
+        height:150,
+        width: 320,
+        alignment: Alignment.center,
+        padding: EdgeInsets.all(padding),
+        child: Row(
+           crossAxisAlignment: CrossAxisAlignment.stretch,
+           children: <Widget>[
+             Text('A doctor, \n like anyone else who has \n to deal with human beings,\n each of them unique, \n cannot be a scientist;\n  he is either,like the surgeon,\n a craftsman,\n or, like the physician and\n the psychologist,\n an artist.'),
+             SizedBox(width: 20.0,),
+             Icon(Icons.health_and_safety,size:80,)
+           ],
+        ),
+    );
+  }
 }
